3675d44988771889ea12bb83ab4e856dac7db03a
btester271828 <bryan.tester@strath.ac.uk>
2014-10-15 14:27:29 +0100

    [WIP] Bouncy Balls
    Fixed collisions and added rendering!

--- MATLAB/bouncy_balls.m
+++ MATLAB/bouncy_balls.m
@@ -2,38 +2,36 @@ clear all
 clc
 
 %% Initial positions
-r(1,:) = [1.4947e11 0 0];
-r(2,:) = r(1,:) + [3.829545e8 0 0];
-r(3,:) = r(1,:) + [1.914773e8 -3.316483e8 0];
-r(4,:) = [0 0 0];
+r(1,:) = [0.5 0 0];
+r(2,:) = [-0.5 0 0];
+
 %% Initial velocities
-v(1,:) = [0 2.978e4 0];
-v(2,:) = v(1,:) + [0 1.02e3 0]; 
-v(3,:) = v(1,:) + [883.3459 510 0];
-v(4,:) = [0 0 0];
+v(1,:) = [0 0 0];
+v(2,:) = [1 0 0];
+
 %% Initial angular velocities
 w = [0 0 0; 0 0 0; 0 0 0];
 %% Charges
 q(1) = 0;
 q(2) = 0;
-q(3) = 0;
-q(4) = 0;
+
 %% Radii
-rad(1) = 6371e3;
-rad(2) = 1737e3;
-rad(3) = 1;
-rad(4) = 696342e3;
+rad(1) = 0.05;
+rad(2) = 0.05;
+
 %% Masses
-m = [5.97e24 7.35e22 10 1.98855e30];
+m = [1 1];
 
 %% Settings
-do_translate = 1;
+do_translate = 0;
 frame = 1;
-delta_t = 60;
+delta_t = 0.0001;
 F_tol = 0.01;
 v_tol = 0.01;
 k = (4*pi*8.85419e-12)^-1;
 G = 6.67384e-11;
+max_step = 20480;
+warp = 1;
 
 
 %% Dynamics iterations
@@ -41,9 +39,13 @@ G = 6.67384e-11;
 F_total = Inf;
 t_step = 0;
 
-while (t_step < 1051200)
-    clc
-    disp([num2str(t_step*100/1051200),'%'])
+
+h = waitbar(0,'Simulating...');
+
+while (t_step < max_step)
+    
+    waitbar(t_step/max_step)
+    
     t_step = t_step + 1;
     F = zeros(size(r));
     F_norm = zeros(size(r,1),1);
@@ -53,11 +55,11 @@ while (t_step < 1051200)
         F_delta = zeros(size(r,1),3);
         j_delta = zeros(size(r,1),3);
         r_temp = r(i,:);
-        parfor ii = 1:size(r,1)
+        for ii = 1:size(r,1)
             if ii ~= i
-                R = r_temp - r(ii,:);               
+                R = r_temp - r(ii,:);
                 F_delta(ii,:) = (q(i)*q(ii)*k/dot(R,R) - m(i)*m(ii)*G/dot(R,R))*R*(1-heaviside(rad(i) + rad(ii) - norm(R)))/norm(R);
-                j_delta(ii,:) = 2*R*heaviside(rad(i) + rad(ii) - norm(R))*dot((v(i,:)-v(ii,:)),R)/(norm(R)^2*(m(i)^-1 + m(ii)^-1));
+                j_delta(ii,:) = -2*R*heaviside(rad(i) + rad(ii) - norm(R))*dot((v(ii,:)-v(i,:)),R)/(norm(R)^2*(m(i)^-1 + m(ii)^-1));
             end
         end
         
@@ -67,9 +69,11 @@ while (t_step < 1051200)
     end
     
     for i = 1:size(r,1)
+        
+        v(i,:) = v(i,:) + F(i,:)*delta_t/m(i) - j(i,:)/m(i);
         r(i,:) = r(i,:) + v(i,:)*delta_t;
         r_tracker(t_step,i,:) = r(i,:);
-        v(i,:) = v(i,:) + F(i,:)*delta_t/m(i) + j(i,:)/m(i);
+        
     end
     
     t(t_step) = t_step * delta_t;
@@ -78,6 +82,9 @@ while (t_step < 1051200)
     
     
 end
+
+close(h)
+
 if do_translate
     for i=1:size(r_tracker,1)
         for j=1:size(r_tracker,3)
@@ -85,6 +92,36 @@ if do_translate
         end
     end
 end
-    % plot(r_tracker(:,2,1),r_tracker(:,2,2),r_tracker(:,3,1),r_tracker(:,3,2))
-    plot(t,r_tracker(:,1,1),t,r_tracker(:,2,1),t,r_tracker(:,3,1))
-    % plot(r_tracker(:,2,1),r_tracker(:,2,2),0.1*cos((1:65)/30*pi),0.1*sin((1:65)/30*pi))
+
+frameset = zeros(1920,1080,ceil(max_step*delta_t*30/warp));
+r_scaled = zeros(size(r_tracker,1),size(r_tracker,2),2);
+
+
+xmin = min(min(r_tracker(:,:,1))) - 2*max(rad);
+ymin = min(min(r_tracker(:,:,2))) - 2*max(rad);
+xmax = max(max(r_tracker(:,:,1))) + 2*max(rad);
+ymax = max(max(r_tracker(:,:,2))) + 2*max(rad);
+scale = max(1920/(xmax-xmin),1080/(ymax-ymin));
+
+r_scaled(:,:,1) = int16((r_tracker(:,:,1) - xmin)*scale);
+r_scaled(:,:,2) = int16((r_tracker(:,:,2) - ymin)*scale);
+
+circ = vision.ShapeInserter;
+circ.Fill = true;
+circ.Shape = 'Circles';
+circ.Antialiasing = true;
+
+for t = 1:size(frameset,3)
+    t_ind = min(ceil(delta_t*30/warp)*t,size(r_tracker,1));
+   balls = zeros(size(r,1),3);
+    for i = 1:size(r,1)
+      balls(i,:) = [r_scaled(t_ind,i,1),r_scaled(t_ind,i,2),rad(i)];
+    end
+    
+   frameset(:,:,t) = step(circ,frameset(:,:,t),balls);
+   
+end
+
+% plot(r_tracker(:,2,1),r_tracker(:,2,2),r_tracker(:,3,1),r_tracker(:,3,2))
+plot(t,r_tracker(:,1,1),t,r_tracker(:,2,1))
+% plot(r_tracker(:,2,1),r_tracker(:,2,2),0.1*cos((1:65)/30*pi),0.1*sin((1:65)/30*pi))
