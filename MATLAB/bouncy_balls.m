f5609bbe32881608606783380caed9287becddea
btester271828 <bryan.tester@strath.ac.uk>
2014-10-22 17:23:12 +0100

    [WIP] Bouncy Balls

--- MATLAB/bouncy_balls.m
+++ MATLAB/bouncy_balls.m
@@ -1,14 +1,14 @@
 clear all
 
-r(1,:) = [1.4947e11 0 0];
-r(2,:) = r(1,:) + [3.829545e8 0 0];
-r(3,:) = r(1,:) + [1.914773e8 -3.316483e8 0];
-r(4,:) = [0 0 0];
+
+r(1,:) = [0 0 0];
+r(2,:) = [1 0 0];
+
 %% Initial velocities
-v(1,:) = [0 2.978e4 0];
-v(2,:) = v(1,:) + [0 1.02e3 0]; 
-v(3,:) = v(1,:) + [883.3459 510 0];
-v(4,:) = [0 0 0];
+
+v(1,:) = [0 0 0];
+v(2,:) = [-1 0 0]; 
+
 %% Initial angular velocities
 w = [0 0 0; 0 0 0; 0 0 0; 0 0 0];
 %% Charges
@@ -17,27 +17,26 @@ q(2) = 0;
 q(3) = 0;
 q(4) = 0;
 %% Radii
-rad(1) = 6371e3;
-rad(2) = 1737e3;
-rad(3) = 1;
-rad(4) = 696342e3;
+rad(1) = 0.1;
+rad(2) = 0.1;
+
 %% Masses
-m = [5.97e24 7.35e22 10 1.98855e30];
+m = [1 1];
 
 %% Settings
-delta_t = 120; % Delta t for integration
+delta_t = 0.00004; % Delta t for integration
 grad_correct = 1; % Use gradient term in integration in addition to linear
 k = (4*pi*8.85419e-12)^-1; % Strength of Electrostatic (Coulomb) interactions
 G = 6.67384e-11; % Strength of Gravitiational (Newtonian) interactions
 epsilon = 1e-6; % Strength of LJ interactions
 r_m = 0.3; % Range of LJ
-max_step = 4e5; % Number of time steps simulated
+max_step = 5e4; % Number of time steps simulated
 mu_s = 0.5; % Coefficient of Static Friction
 mu_d = 0.4; % Coefficient of Dynamic Friction
 diss = 1; % Coefficient of Restitution
 %% Render options
-do_render = 1;
-warp = 5e5; % Ratio of simulated time to rendered video time
+do_render = 0;
+warp = 4e4; % Ratio of simulated time to rendered video time
 do_translate = 1;
 frame = 2;
 
@@ -48,8 +47,8 @@ t_step = 0;
 
 E_start = [0 0];
 for i = 1 :size(r,1)
-    I(i) = (2*m(i)*(rad(i)^2)/5);
     
+    I(i) = (2*m(i)*(rad(i)^2)/5);
     E_start = E_start + 0.5*(dot(v(i,:),v(i,:))*m(i)*[1 0] + dot(w(i,:),w(i,:))*I(i)*[0 1]);
     for ii = 1:size(r,1)
         mass(i,ii) = m(i)*m(ii)*(1 - (i == ii));
@@ -86,13 +85,10 @@ while (t_step < max_step)
             
             %% Force from ranged particle interactions
             F_part(:,i,ii) = (-charge(i,ii)*k/d(i,ii)^2 + mass(i,ii)*G/d(i,ii)^2 + epsilon*(r_m^12/d(i,ii)^13 - 2*r_m^6/d(i,ii)^7))*R(:,i,ii)*(1-collision_flag)/d(i,ii);
-            if grad_correct
+         
                 F_part_grad(:,i,ii) = (2*charge(i,ii)*k/d(i,ii)^3 - 2*mass(i,ii)*G/d(i,ii)^3 - epsilon*(-13*r_m^12/d(i,ii)^14 + 14*r_m^6/d(i,ii)^8))*...
                     R(:,i,ii)*(1-collision_flag)/d(i,ii)*dot((v(ii,:) - v(i,:)),R(:,i,ii)/d(i,ii));
-            else
-                F_part_grad(:,i,ii) = 0;
-            end
-            
+                      
             F_part(:,ii,i) = -F_part(:,i,ii);
             F_part_grad(:,ii,i) = -F_part_grad(:,i,ii);
             
