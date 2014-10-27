4ae7f49c17e2a5f4058446f177e2b84abe98b2dd
btester271828 <bryan.tester@strath.ac.uk>
2014-10-27 11:34:59 +0000

    [WIP] Kernel

--- Julia/F_kernel.jl
+++ Julia/F_kernel.jl
@@ -12,13 +12,12 @@ const F_kernel = "
 			  	 	__global const float *massrecip,
 			  	 	__global const float *inertiapair,
 				 	__global const float *rad,	
-				 	__global const float *dt,			  
+				 	__global const float *stuff,			  
 				 	__global float3 *r,
 				 	__global float3 *v,
 				 	__global float3 *w)
 				  
-		{ 	
-			int diss = 1;
+		{ 			
 			int x = get_global_id(0);	
 			float3 r1 = (r[1,lookup[x,1]],r[2,lookup[x,1]],r[3,lookup[x,1]]);
 			float3 r2 = (r[1,lookup[x,2]],r[2,lookup[x,2]],r[3,lookup[x,2]]);
@@ -33,77 +32,36 @@ const F_kernel = "
 			float3 vtemp = v1 - v2; 
 			float p = dot(vtemp,Runit);
 		
-			int collisionflag = 1/2 + (radvec[x] - d)/(2*(radvec[x] - d));	
+			float collisionflag = 1/2 + (radvec[x] - d)/(2*(radvec[x] - d));	
 		
 			float F = ((massvec[x] - chargevec[x])/(d*d) + 0)*(1 - collisionflag);				
 			float dF = ((-2*massvec[x] + 2*chargevec[x])/(d*d*d) + 0)*(1 - collisionflag)*p;		
 			
 			float3 v_rel = (vtemp - p*Runit) -cross(wvec,Runit);
 			float jf = -collisionflag/(inertiapair[x] + massrecip[x]);
-			float j =  -(1 + diss)*collisionflag*p/massrecip[x];					
+			float j =  -(1 + stuff[2])*collisionflag*p/massrecip[x];					
 					
-			v2 += F*Runit*dt/m[lookup[x,2]] + dF*Runit*dt*dt/m[lookup[x,2]] - j*Runit/m[lookup[x,2]] - jf*v_rel/m[lookup[x,2]];
-			v1 += -F*Runit*dt/m[lookup[x,1]] - dF*Runit*dt*dt/m[lookup[x,1]] + j*Runit/m[lookup[x,1]] + jf*v_rel/m[lookup[x,1]];
-			w2 += float3 -1*cross(Runit,jf*v_rel)*rad[lookup[x,2]]/I[lookup[x,2]];
-			w1 += float3 cross(Runit,jf*v_rel)[1]*rad[lookup[x,1]]/I[lookup[x,1]];
-		}
-"		
-const a_kernel = "
-		__kernel void Fimp(__global const float *chargevec,
-				 	__global const float *m,				  
-				  	__global const float *I,
-				  	__global const int *lookup,
-			  	 	__global const float *massvec,			
-				 	__global const float *radvec,
-			  	 	__global const float *massrecip,
-			  	 	__global const float *inertiapair,
-				 	__global const float *rad,	
-				 	__global const float *dt,			  
-				 	__global float3 *r,
-				 	__global float3 *v,
-				 	__global float3 *w)
-				  
-		{ 	
-			float diss = 1;
-			int x = get_global_id(0);	
-			float3 r1 = (r[1,lookup[x,1]],r[2,lookup[x,1]],r[3,lookup[x,1]]);
-			float3 r2 = (r[1,lookup[x,2]],r[2,lookup[x,2]],r[3,lookup[x,2]]);
-			float3 v1 = (v[1,lookup[x,1]],v[2,lookup[x,1]],v[3,lookup[x,1]]);
-			float3 v2 = (v[1,lookup[x,2]],v[2,lookup[x,2]],v[3,lookup[x,2]]);
-			float3 w1 = (w[1,lookup[x,1]],w[2,lookup[x,1]],w[3,lookup[x,1]]);
-			float3 w2 = (w[1,lookup[x,2]],w[2,lookup[x,2]],w[3,lookup[x,2]]);
-			
-			float d = distance(r1, r2);
-		 	float3 Runit = normalize(r1 - r2);
-			float3 wvec = w1*rad[lookup[x,1]] + w2*rad[lookup[x,2]];
-			float3 vtemp = v1 - v2; 
-			float p = dot(vtemp,Runit);
-			float collisionflag = 1/2 + (radvec[x] - d)/(2*(radvec[x] - d));
-  			float F = ((massvec[x] - chargevec[x])/(d*d) + 0)*(1 - collisionflag);				
-			float dF = ((-2*massvec[x] + 2*chargevec[x])/(d*d*d) + 0)*(1 - collisionflag)*p;
-			
-			float3 v_rel = (vtemp - p*Runit) - cross(wvec,Runit);
-			float jf = -collisionflag/(inertiapair[x] + massrecip[x]);
-			float j =  -(1 + diss)*collisionflag*p/massrecip[x];
-			
+			v2 += F*Runit*stuff[1]/m[lookup[x,2]] + dF*Runit*stuff[1]*stuff[1]/m[lookup[x,2]] - j*Runit/m[lookup[x,2]] - jf*v_rel/m[lookup[x,2]];
+			v1 += -F*Runit*stuff[1]/m[lookup[x,1]] - dF*Runit*stuff[1]*stuff[1]/m[lookup[x,1]] + j*Runit/m[lookup[x,1]] + jf*v_rel/m[lookup[x,1]];
 			w2 += -1*cross(Runit,jf*v_rel)*rad[lookup[x,2]]/I[lookup[x,2]];
 			w1 += cross(Runit,jf*v_rel)[1]*rad[lookup[x,1]]/I[lookup[x,1]];
-		}	
+		}
 "		
+
 const r_kernel = " 
-		__kernel void rstep(__global const float *dt,
+		__kernel void rstep(__global const float *stuff,
 					__global float3 *v,
 					__global float3 *r)
 		{
 			int x = get_global_id(0);
-			r[1,x] += v[1,x]*dt[1];
-			r[2,x] += v[2,x]*dt[1];
-			r[3,x] += v[3,x]*dt[1];
+			r[1,x] += v[1,x]*stuff[1];
+			r[2,x] += v[2,x]*stuff[1];
+			r[3,x] += v[3,x]*stuff[1];
 		}
 "
 
 rstep = cl.Program(ctx, source=r_kernel) |> cl.build!
 ker1 = cl.Kernel(rstep, "rstep");
-dynamics = cl.Program(ctx, source=a_kernel) |> cl.build!
+dynamics = cl.Program(ctx, source=F_kernel) |> cl.build!
 ker2 = cl.Kernel(dynamics, "Fimp");
 	
--- Julia/bouncy_balls_cl.jl
+++ Julia/bouncy_balls_cl.jl
@@ -11,33 +11,36 @@ const r_m = 0.3;
 
 fileread("setup.vec");
 
+
+global const stuff = float32([0,0]);
+
 try
-	global const warp = float64(settings[6]);	
-	global const mu_d = float64(settings[5]);	
-	global const max_step = int64(settings[1]);
-	global const dt = float64(settings[2]);
-	global const diss = float64(settings[3]);
-	global const mu_s = float64(settings[4]);	
+	global const warp = float32(settings[6]);	
+	global const mu_d = float32(settings[5]);	
+	global const max_step = int32(settings[1]);
+	stuff[1] = float32(settings[2]);
+	stuff[2] = float32(settings[3]);
+	global const mu_s = float32(settings[4]);	
 catch
 	print("Settings not present or invalid; using defaults\n")
-	global const max_step = int64(5e4);
-	global const delta_t = float64(0.05);
-	global const diss = float64(1);
-	global const warp = float64(1);
+	global const max_step = int32(5e4);
+	stuff[1] = float32(0.05);
+	stuff[2] = float32(1);
+	global const warp = float32(1);
 end
 
-global t_step = int64(0);
-const n = int64(size(r,2));
-const n_el = int64(1/2*n*(n-1));
-global r_tracker = zeros(3,n,max_step);	
+global t_step = int32(0);
+const n = int32(size(r,2));
+const n_el = int32(1/2*n*(n-1));
+global r_tracker = float32(zeros(3,n,max_step));	
 
-global I = zeros(size(r,2),1);
-global massvec = zeros(n_el,1);
-global chargevec = zeros(n_el,1);
-global radvec = zeros(n_el,1);
-global massrecip = zeros(n_el,1);
-global inertiapair = zeros(n_el,1);
-global lookup = float32(zeros(n_el,2));
+global I = float32(zeros(size(r,2),1));
+global massvec = float32(zeros(n_el,1));
+global chargevec = float32(zeros(n_el,1));
+global radvec = float32(zeros(n_el,1));
+global massrecip = float32(zeros(n_el,1));
+global inertiapair = float32(zeros(n_el,1));
+global lookup = int32(zeros(n_el,2));
 
 for x = 1:n
 	I[x] = (2*m[x]*(rad[x]^2)/5);
@@ -45,26 +48,27 @@ end
 
 for x = 2:n
 	for y = 1:(x-1)	
-	local i = int64(0.5*(x-1)*(x-2) + y);	
-		massvec[i] = m[x]*m[y]*G;
-		chargevec[i] = q[x]*q[y]*k;
-		radvec[i] = rad[x] + rad[y];
+	local i = int32(0.5*(x-1)*(x-2) + y);	
+		massvec[i] = float32(m[x]*m[y]*G);
+		chargevec[i] = float32(q[x]*q[y]*k);
+		radvec[i] = float32(rad[x] + rad[y]);
 		massrecip[i] = (1/m[x] + 1/m[y]);
 		inertiapair[i] = rad[y]^2/I[y] + rad[x]^2/I[x];
 		lookup[i,:] = [x y];
 	end
 end
 
-tbuff = cl.Buffer(Float32, ctx, (:r, :copy), hostbuf=float32(dt));
-cbuff = cl.Buffer(Float32, ctx, (:r, :copy), hostbuf=float32(chargevec));
-m1buff = cl.Buffer(Float32, ctx, (:r, :copy), hostbuf=float32(m));
-m2buff = cl.Buffer(Float32, ctx, (:r, :copy), hostbuf=float32(massvec));
-m3buff = cl.Buffer(Float32, ctx, (:r, :copy), hostbuf=float32(massrecip));
-r1buff = cl.Buffer(Float32, ctx, (:r, :copy), hostbuf=float32(rad));
-r2buff = cl.Buffer(Float32, ctx, (:r, :copy), hostbuf=float32(radvec));
-I1buff = cl.Buffer(Float32, ctx, (:r, :copy), hostbuf=float32(I));
-I2buff = cl.Buffer(Float32, ctx, (:r, :copy), hostbuf=float32(inertiapair));
 lbuff = cl.Buffer(Int32, ctx, (:r, :copy), hostbuf=lookup);
+cbuff = cl.Buffer(Float32, ctx, (:r, :copy), hostbuf=chargevec);
+m1buff = cl.Buffer(Float32, ctx, (:r, :copy), hostbuf=float32(m));
+m2buff = cl.Buffer(Float32, ctx, (:r, :copy), hostbuf=massvec);
+m3buff = cl.Buffer(Float32, ctx, (:r, :copy), hostbuf=massrecip);
+r1buff = cl.Buffer(Float32, ctx, (:r, :copy), hostbuf=float32(rad));
+r2buff = cl.Buffer(Float32, ctx, (:r, :copy), hostbuf=radvec);
+I1buff = cl.Buffer(Float32, ctx, (:r, :copy), hostbuf=I);
+I2buff = cl.Buffer(Float32, ctx, (:r, :copy), hostbuf=inertiapair);
+tbuff = cl.Buffer(Float32, ctx, (:r, :copy), hostbuf=stuff);
+
 
 rbuff = cl.Buffer(Float32, ctx, (:rw, :copy), hostbuf=float32(r));
 vbuff = cl.Buffer(Float32, ctx, (:rw, :copy), hostbuf=float32(v));
@@ -73,9 +77,9 @@ wbuff = cl.Buffer(Float32, ctx, (:rw, :copy), hostbuf=float32(w));
 p = Progress(max_step,1)
 for t_step = 1:max_step			
 	
-	cl.call(queue, kerF, n_el, nothing, cbuff, m1buff, I1buff, lbuff, m2buff, r2buff, m3buff, I2buff, r1buff, tbuff, rbuff, vbuff, wbuff); 
+	cl.call(queue, ker2, n_el, nothing, cbuff, m1buff, I1buff, lbuff, m2buff, r2buff, m3buff, I2buff, r1buff, tbuff, rbuff, vbuff, wbuff); 
 
-	cl.call(queue, kerR, n, nothing, tbuff, vbuff, rbuff);	
+	cl.call(queue, ker1, n, nothing, tbuff, vbuff, rbuff);	
 					
       	r_tracker[:,:,t_step] = cl.read(queue, rbuff);	
 	
@@ -84,9 +88,9 @@ end
 
 print("Simulation complete!\n")
 
-global frameset = zeros(3,n,int64(ceil(max_step*delta_t*30/warp))+1);
+global frameset = float32(zeros(3,n,int32(ceil(max_step*delta_t*30/warp))+1);
 
-for t = 1:int64(ceil(max_step*delta_t*30/warp))
+for t = 1:int32(ceil(max_step*delta_t*30/warp))
     
     local t_ind = min(ceil(max_step/ceil(max_step*delta_t*30/warp)*t),size(r_tracker,3));
     frameset[:,:,t] = r_tracker[:,:,t_ind];
