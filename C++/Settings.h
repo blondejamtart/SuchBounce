43904c77405aec8d02104bea474783e0102162fa
btester271828 <bryan.tester@strath.ac.uk>
2016-02-26 11:45:00 +0000

    Improvements to Multidev
    Variable block size added in settings

--- C++/Settings.h
+++ C++/Settings.h
@@ -13,12 +13,13 @@
 
 //Settings:
 
-const int n = 1024;
+const int n = 2048;
 const double settings[10] = {pow(2,4), pow(2.0,-6), pow(2.0,-6), 1e+00, 2.1e-16, 0, 0, 0, 1e+01, pow(2.0,2)};
 // [ t_max, dt_init, dt_min, soft body energy loss, VdW attraction, VdW repulsion (Deprecated), mu_static, mu_dynamic, soft body parameter, render warp]  
 
 const double max_time = settings[0];
 const double warp = settings[9];
-const int workgroup_size = 2;
+const int workgroup_size = 64;
 const int nDevs = 2;
-int Devs[3] = { 2, 1, 0 };
+int Devs[3] = { 1, 0, 2 };
+int n_block[3] = {512, 511, 256};
